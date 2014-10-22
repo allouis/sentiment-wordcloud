c9b29b360f2848c43ea1c5c1a5290889ffb1a741
Fabien O'Carroll <fabien@allou.is>
2014-10-22 23:04:21 +0100

    Properly scope the positionInCloud function

--- wordcloud.js
+++ wordcloud.js
@@ -6,7 +6,6 @@ var parseTopic = require('./lib/wordcloud/parseTopic');
 var sortByVolume = require('./lib/wordcloud/sortByVolume');
 var setWordSize = require('./lib/wordcloud/setWordSize');
 var createShapes = require('./lib/shape/createShapeObject');
-var positionInCloud = require('./lib/wordcloud/positionInCloud')(cloud);
 var appendTo = require('./lib/dom/appendTo');
 var createSpan = require('./lib/wordcloud/createSpans');
 var setWordColour = require('./lib/wordcloud/setWordColour');
@@ -14,6 +13,7 @@ var setWordColour = require('./lib/wordcloud/setWordColour');
 function wordcloud(opts) {
   var cloudEl = opts.cloudEl;
   var metadataEl = opts.metadataEl;
+  var positionInCloud = require('./lib/wordcloud/positionInCloud')(cloudEl);
   return compose(
     each(positionInCloud),
     map(createShapes),
