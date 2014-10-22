3a0bc0bf57480079a92607d91a1fd6ae885973fc
Fabien O'Carroll <fabien@allou.is>
2014-10-22 22:33:04 +0100

    Add main wordcloud.js file

---
+++ wordcloud.js
@@ -0,0 +1,29 @@
+var each = require('./lib/utils/each');
+var map = require('./lib/utils/map');
+var compose = require('./lib/utils/compose');
+
+var parseTopic = require('./lib/wordcloud/parseTopic');
+var sortByVolume = require('./lib/wordcloud/sortByVolume');
+var setWordSize = require('./lib/wordcloud/setWordSize');
+var createShapes = require('./lib/shape/createShapeObject');
+var positionInCloud = require('./lib/wordcloud/positionInCloud')(cloud);
+var appendTo = require('./lib/dom/appendTo');
+var createSpan = require('./lib/wordcloud/createSpans');
+var setWordColour = require('./lib/wordcloud/setWordColour');
+
+function wordcloud(opts) {
+  var cloudEl = opts.cloudEl;
+  var metadataEl = opts.metadataEl;
+  return compose(
+    each(positionInCloud),
+    map(createShapes),
+    map(appendTo(cloudEl)),
+    map(createSpan(metadataEl)),
+    map(setWordColour),
+    map(setWordSize),
+    sortByVolume,
+    map(parseTopic)
+  );
+}
+
+module.exports = wordcloud;
